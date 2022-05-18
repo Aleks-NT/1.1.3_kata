db892b6c93cfe0e7b33eef02871c87e6b76ea7f1
Aleks-NT <mr.very@yandex.ru>
2022-05-18 23:27:39 +0500

    Доработал

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbtest", "root", "12345");
+    }
+
+
+
+
+
+
     // реализуйте настройку соеденения с БД
 }
